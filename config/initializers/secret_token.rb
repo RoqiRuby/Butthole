dff5cb294f96556fcc9494f149926433810b6aee
Chris <chris@Linz-platz.(none)>
2011-01-03 00:31:52 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Butthole::Application.config.secret_token = 'cede54f04357e815b396ac9431d09470d9f0f5b5041db2a60a6653a9d3508bba8a348470a0e064e8104f5514439ad8aebfd37f3adb5f893a613e6a305c77a579'
